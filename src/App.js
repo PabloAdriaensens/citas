697dbd648ad8391f9343ff38fab8456068e64a73
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-21 22:41:33 +0200

    creating our component Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+
+function Formulario() {
+  return (
+    <Fragment>
+      <h2> Crear Cita </h2>
+
+      <form>
+        <label> Número Mascota </label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-anchura"
+          placeholder="número de Mascota"
+        />
+
+        <label> Número de dueño </ label>
+        <input
+          type="text"
+          name="Propietario"
+          className="u-full-anchura"
+          placeholder="Número de Dueño de la Mascota"
+        />
+
+        <label> Fecha </ label>
+        <input
+          type="fecha"
+          className="u-full-anchura"
+          name="Fecha"
+        />
+
+        <label> Hora </ label>
+        <input
+          type="tiempo"
+          className="u-full-anchura"
+          name="hora"
+        />
+
+        <label> sintomas </ label>
+        <textarea
+          className="u-full-anchura"
+          name="sintomas"
+        > </textarea>
+
+        <button type="submit" className="button-primary u-full-width"> Agregar </ button>
+      </ form>
+    </ Fragment>
+  )
+}
 
 function App() {
   // useState retorna 2 funciones
@@ -7,7 +55,19 @@ function App() {
   const [citas, guardarCita] = useState([]);
 
   return (
-    <h1>hola</h1>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half">
+            <Formulario />
+          </div>
+          <div className="one-half">
+
+          </div>
+        </div>
+      </div>
+    </Fragment>
   )
 }
 

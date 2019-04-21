e46530ea0581e7e67ffbe2faf2a1bbefe504affa
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-21 23:41:26 +0200

    Eliminating appointments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, Fragment } from 'react';
 
-function Cita({ cita }) {
+function Cita({ cita, index, eliminarCita }) {
   return (
     <div className="cita">
       <p>Mascota: <span>{cita.mascota}</span></p>
@@ -8,6 +8,9 @@ function Cita({ cita }) {
       <p>Fecha: <span>{cita.fecha}</span></p>
       <p>Hora: <span>{cita.hora}</span></p>
       <p>Sintomas: <span>{cita.sintomas}</span></p>
+      <button 
+        onClick={() => eliminarCita(index)}
+        type="button" className="button eliminar u-full-width">Eliminar X</button>
     </div>
   )
 }
@@ -119,6 +122,13 @@ function App() {
     guardarCita(nuevasCitas);
   }
 
+  // Elimina las citas del state
+  const eliminarCita = index => {
+    const nuevasCitas = [...citas];
+    nuevasCitas.splice(index, 1);
+    guardarCita(nuevasCitas);
+  }
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -134,6 +144,7 @@ function App() {
                 key={index}
                 index={index}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />
             ))}
           </div>
